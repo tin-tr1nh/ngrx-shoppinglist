556e1c334a79dbbc9167e1ad421f665046e98632
Tin Trinh <trinh.tin@hamee.co.jp>
2020-05-30 23:40:15 +0900

    Add ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { ShoppingReducer } from './store/reducers/shopping.reducer';
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot({shopping: ShoppingReducer})
+    StoreModule.forRoot({shopping: ShoppingReducer}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
