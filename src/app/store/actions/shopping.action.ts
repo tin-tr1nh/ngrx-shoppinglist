258b728da4c92db50e6d8a63092d15592d0bf798
Tin Trinh <trinh.tin@hamee.co.jp>
2020-06-06 17:40:29 +0900

    Add action for loading, error

--- src/app/store/actions/shopping.action.ts
+++ src/app/store/actions/shopping.action.ts
@@ -2,23 +2,84 @@ import {Action} from '@ngrx/store';
 import {ShoppingItem} from '../models/shopping-item.model';
 
 export enum ShoppingActionTypes {
-  ADD_ITEM = '[SHOPPING-LIST] Add item',
-  REMOVE_ITEM = '[SHOPPING-LIST] Remove item'
+  LOAD_SHOPPING = '[SHOPPING] Load Shopping',
+  LOAD_SHOPPING_SUCCESS = '[SHOPPING] Load Shopping Success',
+  LOAD_SHOPPING_FAILURE = '[SHOPPING] Load Shopping Failure',
+  ADD_ITEM = '[SHOPPING] Add Item',
+  ADD_ITEM_SUCCESS = '[SHOPPING] Add Item Success',
+  ADD_ITEM_FAILURE = '[SHOPPING] Add Item Failure',
+  DELETE_ITEM = '[SHOPPING] Delete Item',
+  DELETE_ITEM_SUCCESS = '[SHOPPING] Delete Item Success',
+  DELETE_ITEM_FAILURE = '[SHOPPING] Delete Item Failure'
+}
+
+export class LoadShoppingAction implements Action {
+  readonly type = ShoppingActionTypes.LOAD_SHOPPING;
+}
+
+export class LoadShoppingSuccessAction implements Action {
+  readonly type = ShoppingActionTypes.LOAD_SHOPPING_SUCCESS;
+
+  constructor(public payload: Array<ShoppingItem>) {
+  }
+
+}
+
+export class LoadShoppingFailureAction implements Action {
+  readonly type = ShoppingActionTypes.LOAD_SHOPPING_FAILURE;
+
+  constructor(public payload: Error) {
+  }
 }
 
 export class AddItemAction implements Action {
   readonly type = ShoppingActionTypes.ADD_ITEM;
 
   constructor(public payload: ShoppingItem) {
-
   }
 }
 
-export class RemoveItemAction implements Action {
-  readonly type = ShoppingActionTypes.REMOVE_ITEM;
+export class AddItemSuccessAction implements Action {
+  readonly type = ShoppingActionTypes.ADD_ITEM_SUCCESS;
+
+  constructor(public payload: ShoppingItem) {
+  }
+}
+
+export class AddItemFailureAction implements Action {
+  readonly type = ShoppingActionTypes.ADD_ITEM_FAILURE;
+
+  constructor(public payload: Error) {
+  }
+}
+
+export class DeleteItemAction implements Action {
+  readonly type = ShoppingActionTypes.DELETE_ITEM;
 
   constructor(public payload: string) {
   }
 }
 
-export type ShoppingAction = AddItemAction | RemoveItemAction;
+export class DeleteItemSuccessAction implements Action {
+  readonly type = ShoppingActionTypes.DELETE_ITEM_SUCCESS;
+
+  constructor(public payload: string) {
+  }
+}
+
+export class DeleteItemFailureAction implements Action {
+  readonly type = ShoppingActionTypes.DELETE_ITEM_FAILURE;
+
+  constructor(public payload: string) {
+  }
+}
+
+export type ShoppingAction = AddItemAction |
+    AddItemSuccessAction |
+    AddItemFailureAction |
+    DeleteItemAction |
+    DeleteItemSuccessAction |
+    DeleteItemFailureAction |
+    LoadShoppingAction |
+    LoadShoppingFailureAction |
+    LoadShoppingSuccessAction;
--- src/app/store/reducers/shopping.reducer.ts
+++ src/app/store/reducers/shopping.reducer.ts
@@ -1,24 +1,74 @@
 import {ShoppingItem} from '../models/shopping-item.model';
-import {ShoppingAction, ShoppingActionTypes,} from '../actions/shopping.action';
+import {ShoppingAction, ShoppingActionTypes} from '../actions/shopping.action';
 
-const initialState: Array<ShoppingItem> = [
-  {
-    id: 'abc1233',
-    name: 'Shoe Nike',
-  },
-];
+export interface ShoppingListState {
+  list: Array<ShoppingItem>;
+  loading: boolean;
+  error: Error;
+}
 
-export function ShoppingReducer(
-    state: Array<ShoppingItem> = initialState,
-    action: ShoppingAction
-) {
+const initialState: ShoppingListState = {
+  list: [],
+  loading: false,
+  error: null
+};
+
+export function ShoppingReducer(state: ShoppingListState = initialState, action: ShoppingAction) {
   switch (action.type) {
+    case ShoppingActionTypes.LOAD_SHOPPING:
+      return {
+        ...state,
+        loading: true
+      };
+    case ShoppingActionTypes.LOAD_SHOPPING_SUCCESS:
+      return {
+        ...state,
+        list: action.payload,
+        loading: false
+      };
+
+    case ShoppingActionTypes.LOAD_SHOPPING_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+
     case ShoppingActionTypes.ADD_ITEM:
-      return [...state, action.payload];
-    case ShoppingActionTypes.REMOVE_ITEM:
-      return state.filter(item => item.id !== action.payload);
+      return {
+        ...state,
+        loading: true
+      };
+    case ShoppingActionTypes.ADD_ITEM_SUCCESS:
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+        loading: false
+      };
+    case ShoppingActionTypes.ADD_ITEM_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+    case ShoppingActionTypes.DELETE_ITEM:
+      return {
+        ...state,
+        loading: true
+      };
+    case ShoppingActionTypes.DELETE_ITEM_SUCCESS:
+      return {
+        ...state,
+        list: state.list.filter(item => item.id !== action.payload),
+        loading: false
+      };
+    case ShoppingActionTypes.DELETE_ITEM_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
     default:
       return state;
   }
-
 }
